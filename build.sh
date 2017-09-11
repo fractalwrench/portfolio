b5c4cb96e680b219b3a06f3f65f9ef3257181425
Jamie <fractalwrench@gmail.com>
2017-09-11 21:13:22 +0100

    minify html, js, css, images

--- build.sh
+++ build.sh
@@ -1,2 +1,14 @@
+# Generate files
 rm -rf /public;
 hugo;
+
+# Minify html
+html-minifier --collapse-whitespace --remove-comments --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --remove-tag-whitespace --use-short-doctype --input-dir public --output-dir public --file-ext html
+
+# Minify css
+for f in public/css/*.css; do csso -i $f -o $f; done
+
+# Minify js
+for f in public/js/*.js; do uglifyjs --compress --mangle -o $f -- $f; done
+
+zip -r public.zip public;
