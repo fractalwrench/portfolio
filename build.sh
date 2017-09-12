64e8d57158861cb2c98d96a6570bcc4d6ac4a1ac
Jamie <fractalwrench@gmail.com>
2017-09-12 08:17:27 +0100

    create separate folder for minified build files

--- build.sh
+++ build.sh
@@ -1,14 +1,15 @@
 # Generate files
-rm -rf /public;
+rm -rf /deploy;
+cp -r public deploy
 hugo;
 
 # Minify html
-html-minifier --collapse-whitespace --remove-comments --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --remove-tag-whitespace --use-short-doctype --input-dir public --output-dir public --file-ext html
+html-minifier --collapse-whitespace --remove-comments --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --remove-tag-whitespace --use-short-doctype --input-dir deploy --output-dir deploy --file-ext html
 
 # Minify css
-for f in public/css/*.css; do csso -i $f -o $f; done
+for f in deploy/css/*.css; do csso -i $f -o $f; done
 
 # Minify js
-for f in public/js/*.js; do uglifyjs --compress --mangle -o $f -- $f; done
+for f in deploy/js/*.js; do uglifyjs --compress --mangle -o $f -- $f; done
 
-zip -r public.zip public;
+zip -r deploy.zip deploy;
